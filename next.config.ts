0b1ac33eca620b1913e1801e10dcda6c24cac085
Freemason-12 <maksatlego@gmail.com>
2026-03-09 23:28:27 +0500

    maybe this will fix it

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   basePath: process.env.PAGES_BASE_PATH,
   // assetPrefix: process.env.PAGES_BASE_PATH,
-  assetPrefix: "/alishev-test-app",
+  assetPrefix: `${process.env.PAGES_BASE_PATH}/alishev-test-app/`,
   output: "export",
 };
 
