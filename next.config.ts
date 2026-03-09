e613d183193b4874750d21292a503d3edd358268
Freemason-12 <maksatlego@gmail.com>
2026-03-09 23:20:00 +0500

    changing assetPrefix in hopes of it getting fixed

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: process.env.PAGES_BASE_PATH,
-  assetPrefix: process.env.PAGES_BASE_PATH,
+  // assetPrefix: process.env.PAGES_BASE_PATH,
+  assetPrefix: "/alishev-test-app",
   output: "export",
 };
 
