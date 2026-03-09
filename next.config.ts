ca7e802714f6bd10ba619744e9d6b0dc6ef941eb
Freemason-12 <maksatlego@gmail.com>
2026-03-09 22:39:34 +0500

    added basic configuration for gh-pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: "/alishev-test-app",
+  output: "export",
 };
 
 export default nextConfig;
