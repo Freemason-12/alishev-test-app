c14c81bf2b0d65c98dd8e222aecf73864ea6b718
Freemason-12 <maksatlego@gmail.com>
2026-03-10 00:28:15 +0500

    fixed styles not applying

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: process.env.PAGES_BASE_PATH,
-  // assetPrefix: process.env.PAGES_BASE_PATH,
-  assetPrefix: `${process.env.PAGES_BASE_PATH}/alishev-test-app/`,
+  assetPrefix: process.env.PAGES_BASE_PATH,
   output: "export",
 };
 
